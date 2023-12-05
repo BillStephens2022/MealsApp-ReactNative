d9149cd86329ab7a333d34d0a82a47d0c7144b49
BillStephens2022 <two4onebill@yahoo.com>
2023-12-05 17:16:36 -0500

    set up new MealsOverviewScreen and added react stack navigator to navigate to the screen

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,10 +1,14 @@
 import { StyleSheet, Pressable, View, Text, Platform } from "react-native";
 
-function CategoryGridTile({ title, color }) {
+function CategoryGridTile({ title, color, onPress }) {
   return (
     <View style={styles.gridItem}>
-      <Pressable android_ripple={{color: "#ccc"}} style={({ pressed }) => [styles.button, pressed ? styles.buttonPressed : null]}>
-        <View style={[styles.innerContainer, {backgroundColor: color}]}>
+      <Pressable 
+        android_ripple={{ color: "#ccc" }} 
+        style={({ pressed }) => [styles.button, pressed ? styles.buttonPressed : null]}
+        onPress={onPress}
+        >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
           <Text style={styles.title} >{title}</Text>
         </View>
       </Pressable>
