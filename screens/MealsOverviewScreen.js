e22b172896a3b10001d86a548c1f8766fa915f56
BillStephens2022 <two4onebill@yahoo.com>
2023-12-06 08:17:49 -0500

    updated version of React-Native, added new MealItem component rendered when navigating to MealsOverviewScreen

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, Text, FlatList } from "react-native";
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
+// note that any screen registered as a Stack screen in the App.js gets a couple of props
+// that can be used:  'navigation' and 'route'. So, this is where the 'route' prop is coming
+// from even though you don't see it explicitly passed down.  See 'react-navigation' documentation
+// for more info on properties that accompany these props.
+function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId;
 
-function MealsOverviewScreen() {
-  return <View style={styles.container}>
-    <Text>Meals Overview Screen</Text>
-  </View>
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title}/>
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
 }
 
 export default MealsOverviewScreen;
@@ -14,5 +34,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-  }
-})
+  },
+});
